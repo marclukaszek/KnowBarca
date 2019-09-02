62714e9b9e57bd402c1474fd9cd737513a5a8f96
Marcin Łukaszek <marlukaszek@gmail.com>
2019-09-03 00:02:02 +0200

    test project added

---
+++ KnowBarca.Tests/AppInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace KnowBarca.Tests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .InstalledApp("com.yama.knowbarca")
+                    .StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
