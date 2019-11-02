bb6e53bcac3745b688a9fc8d6609990722e95b6d
Marcin Łukaszek <marlukaszek@gmail.com>
2019-11-02 18:14:11 +0100

    try7

--- KnowBarca.Tests/AppInitializer.cs
+++ KnowBarca.Tests/AppInitializer.cs
@@ -8,7 +8,7 @@ namespace KnowBarca.Tests
     {
         public static IApp StartApp(Platform platform)
         {
-            string path = @"\..\..\..\KnowBarca\KnowBarca.Android\bin\Release\com.yama.knowbarca.apk";
+            string path = @"..\..\..\KnowBarca\KnowBarca.Android\bin\Release\com.yama.knowbarca.apk";
 
             if (platform == Platform.Android)
             {
